7a6d1142cc406c1b5b9eaa03706fb0eb7a32faee
Camila C <camilacparaluppiiiii@gmail.com>
2026-04-23 18:15:14 -0500

    problema 2 parte 2

--- main.py
+++ main.py
@@ -1,3 +1,12 @@
 """Laboratorio 8 - CLI del gestor de tareas."""
 
 # TODO: Implementar CLI según README.md
+import sys
+from todo_manager import read_todo_file, write_todo_file
+if len(sys.argv) >= 2:
+    try:
+        nombrescript = sys.argv[0]
+        rutaarchivo = sys.argv[1]
+        print("{[sys.argv]}")
+    except IndexError:
+        print("Insufficient arguments provided!")
