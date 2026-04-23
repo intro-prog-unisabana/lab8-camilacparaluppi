5cec8761ab12cfc1247e1fa61f70b87fec6736b6
Camila C <camilacparaluppiiiii@gmail.com>
2026-04-23 17:14:31 -0500

    problema 2 parte 1

--- todo_manager.py
+++ todo_manager.py
@@ -4,7 +4,12 @@
 def read_todo_file(file_path):
     """Reads tasks from a file. Returns a list of tasks."""
     # TODO: Implementar manejo de FileNotFoundError según README.md
-    raise NotImplementedError
+file_path = []
+    try:
+        with open(file_path, 'r') as file:
+    except FileNotFoundError:
+        print(f"File {file_path} not found! Returning an empty to-do list.")
+return file_path
 
 
 def write_todo_file(file_path, tasks):
