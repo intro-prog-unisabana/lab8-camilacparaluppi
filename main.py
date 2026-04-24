235e6343478e9ca6216d5de5f76aa9f3d9db8166
Camila C <camilacparaluppiiiii@gmail.com>
2026-04-23 19:53:23 -0500

    problema 2 parte 3

--- main.py
+++ main.py
@@ -3,17 +3,17 @@
 # TODO: Implementar CLI según README.md
 import sys
 from todo_manager import read_todo_file
-if len(sys.argv) < 2: 
+if len(sys.argv) < 2:
     print("Insufficient arguments provided!")
 elif len(sys.argv) == 2:
-    print("Usage: python main.py <file_path> <command> [arguments]...")
+    pass
 else:
     file_path = sys.argv[1]
     command = sys.argv[2]
-    if command == "view":  
+    if command == "view":
         tasks = read_todo_file(file_path)
         print("Tasks:")
         for task in tasks:
-            print(task)    
-        else:
-            print("Command not found!")
+            print(task)
+    else:
+        print("Command not found!")
