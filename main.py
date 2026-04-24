e940e74d284aaf4598a5a64d5232f25fe6a59d58
Camila C <camilacparaluppiiiii@gmail.com>
2026-04-23 19:49:31 -0500

    problema 2 parte 3

--- main.py
+++ main.py
@@ -5,15 +5,15 @@ import sys
 from todo_manager import read_todo_file
 if len(sys.argv) < 2: 
     print("Insufficient arguments provided!")
+elif len(sys.argv) == 2:
     print("Usage: python main.py <file_path> <command> [arguments]...")
 else:
     file_path = sys.argv[1]
-    if len(sys.argv) > 2: 
-        command = sys.argv[2]
-        if command == "view":  
-                tasks = read_todo_file(file_path)
-                print("Tasks:")
-                for task in tasks:
-                    print(task)    
+    command = sys.argv[2]
+    if command == "view":  
+        tasks = read_todo_file(file_path)
+        print("Tasks:")
+        for task in tasks:
+            print(task)    
         else:
             print("Command not found!")
